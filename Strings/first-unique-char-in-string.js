99f6b9c406a4857444996e77d116ab3ab98fcecc
Hila Caspi <caspi.home@gmail.com>
2021-06-26 11:45:50 -0700

    find the first unique character in a string

---
+++ Strings/first-unique-char-in-string.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+    let counterSingle = {};
+    let counterRepeate = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (counterRepeate[s[i]]) continue;
+        else if (counterSingle[s[i]] >= 0) {
+            delete counterSingle[s[i]];
+            counterRepeate[s[i]] = 1;
+        } else {
+            counterSingle[s[i]] = i;
+        }
+    }
+
+    for (let count in counterSingle) {
+        return counterSingle[count];
+    }
+    return -1;
+};
