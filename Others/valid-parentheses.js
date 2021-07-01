f3e6d4694fb09c6c089bd3f8d05e30952230863f
Hila Caspi <caspi.home@gmail.com>
2021-07-01 10:59:01 -0700

    valid parentheses

---
+++ Others/valid-parentheses.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    if (s.length % 2) return false;
+    const charArr = [];
+    for (let i = 0; i < s.length; i++) {
+        let delta = s[i].charCodeAt() - charArr[charArr.length - 1]
+        if (delta > 0 && delta < 3) {
+            charArr.pop();
+        } else {
+            charArr.push(s[i].charCodeAt());
+        }
+    }
+    return charArr.length === 0;
+};
