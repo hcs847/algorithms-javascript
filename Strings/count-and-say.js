c197b0efe8859c08c09a3ca5afb35b2187734a4d
Hila Caspi <caspi.home@gmail.com>
2021-07-03 10:44:31 -0700

    strings count and say

---
+++ Strings/count-and-say.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function (n) {
+    if (n === 1) return '1';
+    const getCountNSay = (result, number, prev) => {
+        if (number === n) return prev;
+        let i = 0;
+        let count = 1;
+        while (i < prev.length) {
+            if (prev[i] === prev[i + 1]) {
+                count++;
+                i++;
+            } else {
+                result += `${(count)}${prev[i]}`;
+                i++;
+                count = 1;
+            }
+        }
+        return getCountNSay('', number + 1, result);
+    }
+
+    return getCountNSay('', 1, '1');
+
+};
